19cb52a2794caeec4b228048f0768153239cb5ee
David King <David@kinglawnc.com>
2023-07-25 10:59:04 -0400

    Your commit message

--- src/pages/newpage.jsx
+++ src/pages/newpage.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function NewPage() {
     return (
-      <>
+      <><script src="flutter.js" defer></script>
         <Head>
           <title>Ethical Wiki - A better way</title>
           <meta
@@ -11,6 +11,8 @@ export default function NewPage() {
           />
         </Head>
         <main>
+        <iframe src="https://app.flutterflow.io/run/5jwJfbDnoojIn0X0NbLJ"></iframe>
+        
         <iframe width="100%" height="1000px" frameborder="0" allow="clipboard-write;camera;geolocation;fullscreen" src="https://budibase.katyusha.ws/embed/ethical-wiki-new-page-assessment"></iframe>
         </main>
   
