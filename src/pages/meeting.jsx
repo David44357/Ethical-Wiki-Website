b6dc329ea5b36e6ce854a369e128582fda786172
David44357 <David44357@gmail.com>
2023-09-14 19:56:06 -0400

    Update meeting.jsx

--- src/pages/meeting.jsx
+++ src/pages/meeting.jsx
@@ -12,30 +12,6 @@ export default function NewPage() {
         </Head>
         <main>
 
-
-
-/* First make sure that you have installed the package */
-  
-  /* If you are using yarn */
-  // yarn add @calcom/embed-react
-  
-  /* If you are using npm */
-  // npm install @calcom/embed-react
-  
-  import { getCalApi } from "@calcom/embed-react";
-  import { useEffect } from "react";
-  export default function App() {
-	useEffect(()=>{
-	  (async function () {
-		const cal = await getCalApi();
-		cal("ui", {"styles":{"branding":{"brandColor":"#FF551C"}},"hideEventTypeDetails":false,"layout":"month_view"});
-	  })();
-	}, [])
-	return <button
-	  data-cal-link="ethicalwiki/meeting"
-	  data-cal-config='{"layout":"month_view"}'
-	  >Click me</button>;
-  };
   
 
 
