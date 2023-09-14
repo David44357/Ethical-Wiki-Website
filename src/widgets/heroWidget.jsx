2b47dbf177799abbc592affeb31485778c1cf014
David44357 <David44357@gmail.com>
2023-09-14 17:14:53 -0400

    Update heroWidget.jsx

--- src/widgets/heroWidget.jsx
+++ src/widgets/heroWidget.jsx
@@ -52,7 +52,7 @@ export default function HeroWidget() {
             <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
                 <img
                     className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-                    src="/images/team.png"
+                    src="/images/team-1.png"
                     alt=""
                 />
             </div>
