07d1bfe40034131995ef3167dfea0b7a1136d17c
David44357 <David44357@gmail.com>
2023-09-14 17:12:53 -0400

    Update heroWidget.jsx

--- src/widgets/heroWidget.jsx
+++ src/widgets/heroWidget.jsx
@@ -41,6 +41,7 @@ export default function HeroWidget() {
                                         className="w-ful brand-bg-light flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md brand-color bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                                     >
                                        <span class="text-xl">New Page Assessment</span>
+                                        
                                     </a>
                                 </div>
                             </div>
