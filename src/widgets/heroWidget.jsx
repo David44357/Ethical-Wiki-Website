4344d0a5eee35556253127b42671936475037a8a
David44357 <David44357@gmail.com>
2023-09-14 18:23:10 -0400

    Update heroWidget.jsx

--- src/widgets/heroWidget.jsx
+++ src/widgets/heroWidget.jsx
@@ -1,7 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 
 
-
 export default function HeroWidget() {
     return (
         <div className="relative bg-white overflow-hidden">
@@ -31,7 +30,7 @@ export default function HeroWidget() {
                                         href="https://cal.com/ethicalwiki/meeting/"
                                         className="w-full brand-bg flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white md:py-4 md:text-lg md:px-10"
                                     >
-                                        <span class="text-xl">Schedule a Consultation</span>
+                                        <span class="text-xl">Schedule a Meeting</span>
                                     </a>
                                 </div>
                                 <div className="mt-3 sm:mt-0 sm:ml-3">
