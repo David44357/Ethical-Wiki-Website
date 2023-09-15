f5cb16cb63614a2a6cc45d08e049dfb520fae517
David44357 <David44357@gmail.com>
2023-09-14 20:09:31 -0400

    Update heroWidget.jsx

--- src/widgets/heroWidget.jsx
+++ src/widgets/heroWidget.jsx
@@ -29,7 +29,7 @@ export default function HeroWidget() {
                             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                                 <div className="rounded-md shadow">
                                     <a
-                                        href="/meeting"
+                                        href="https://cal.com/ethicalwiki/meeting"
                                         className="w-full brand-bg flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white md:py-4 md:text-lg md:px-10"
                                     >
                                         <span class="text-xl">Schedule a Consultation</span>
