94ec2d26b21a6190a705cc2fce69b7dd879d027f
David44357 <David44357@gmail.com>
2023-09-14 20:16:50 -0400

    Update meeting.jsx

--- src/pages/meeting.jsx
+++ src/pages/meeting.jsx
@@ -11,8 +11,7 @@ export default function NewPage() {
           />
         </Head>
         <main>
-
-
+        <iframe src="https://calendly.com/davidaking/wiki" width="100%" height="1500px"></iframe>
         
          </main>
   
