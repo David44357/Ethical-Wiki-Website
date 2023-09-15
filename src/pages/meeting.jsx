7561f77bd7283f474f940aa917a8ed3d92c1900d
David44357 <David44357@gmail.com>
2023-09-14 20:12:32 -0400

    Update meeting.jsx

--- src/pages/meeting.jsx
+++ src/pages/meeting.jsx
@@ -11,7 +11,31 @@ export default function NewPage() {
           />
         </Head>
         <main>
-        <iframe src="https://calendly.com/davidaking/wiki" width="100%" height="1500px"></iframe>
+
+        /* First make sure that you have installed the package */
+  
+  /* If you are using yarn */
+  // yarn add @calcom/embed-react
+  
+  /* If you are using npm */
+  // npm install @calcom/embed-react
+  
+  import Cal, { getCalApi } from "@calcom/embed-react";
+  import { useEffect } from "react";
+  export default function MyApp() {
+	useEffect(()=>{
+	  (async function () {
+		const cal = await getCalApi();
+		cal("ui", {"styles":{"branding":{"brandColor":"#FF551C"}},"hideEventTypeDetails":false,"layout":"month_view"});
+	  })();
+	}, [])
+	return <Cal
+	  calLink="ethicalwiki/meeting"
+	  style={{width:"100%",height:"100%",overflow:"scroll"}}
+	  config={{layout: 'month_view'}}
+	/>;
+  };
+  
         
          </main>
   
