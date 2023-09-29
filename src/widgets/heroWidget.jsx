cd730036df2301b411a5d936d07ca2bac1068bb4
David44357 <David44357@gmail.com>
2023-09-29 09:52:36 -0400

    Update heroWidget.jsx

--- src/widgets/heroWidget.jsx
+++ src/widgets/heroWidget.jsx
@@ -37,7 +37,7 @@ export default function HeroWidget() {
                                 </div>
                                 <div className="mt-3 sm:mt-0 sm:ml-3">
                                     <a
-                                        href="https://new-page-assessment-6p802b.flutterflow.app/"
+                                        href="https://ew-new-page-assessment.flutterflow.app/"
                                         className="w-ful brand-bg-light flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md brand-color bg-indigo-100 md:py-4 md:text-lg md:px-10"
                                     >
                                        <span class="text-xl">New Page Assessment</span>
