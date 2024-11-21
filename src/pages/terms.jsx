7a31ff4aa69e9638f43481025d53062f82286857
David44357 <David44357@gmail.com>
2024-11-21 14:14:47 -0500

    Update terms.jsx

--- src/pages/terms.jsx
+++ src/pages/terms.jsx
@@ -1,17 +1,4 @@
-import Head from 'next/head'
 
-import { CallToAction } from '@/components/CallToAction'
-import { Faqs } from '@/components/About'
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
-import { Hero } from '@/components/Hero'
-import { Pricing } from '@/components/Pricing'
-import { PrimaryFeatures } from '@/components/PrimaryFeatures'
-import { Reviews } from '@/components/Reviews'
-import { SecondaryFeatures } from '@/components/SecondaryFeatures'
-import { Container } from '@/components/Container'
-
-export default function DiyApps() {
     return (
         <>
             <Head>
