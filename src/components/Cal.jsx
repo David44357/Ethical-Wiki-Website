88ff4b0623eb50e5ee5530979446d09a74478a9f
David44357 <David44357@gmail.com>
2025-03-14 16:14:41 -0400

    Update Cal.jsx

--- src/components/Cal.jsx
+++ src/components/Cal.jsx
@@ -3,12 +3,10 @@
 // yarn add @calcom/embed-react
 /* If you are using npm */
 // npm install @calcom/embed-react
-import Cal,{ getCalApi } from "@calcom/embed-react";
-import { useEffect,useRef } from "react";
-import { Button } from './Button'
 
-
-export default function MyApp() {
+import Cal, { getCalApi } from "@calcom/embed-react";
+  import { useEffect } from "react";
+  export default function MyApp() {
 	useEffect(()=>{
 	  (async function () {
 		const cal = await getCalApi({"namespace":"meeting"});
@@ -17,9 +15,10 @@ export default function MyApp() {
 	}, [])
 	return <Cal namespace="meeting"
 	  calLink="ethicalwiki/meeting"
-	  style={{width:"100%",height:"100%",overflow:"scroll",margin: "20px"}}
+	  style={{width:"100%",height:"100%",overflow:"scroll",margin:"20px"}}
 	  config={{"layout":"month_view"}}
+    
+	  
 	/>;
-	
   };
   
