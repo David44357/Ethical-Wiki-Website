d83a9f02c1b1b3620e8018107208328b4a407b3f
David44357 <David44357@gmail.com>
2025-03-13 21:01:40 -0400

    Update meeting.jsx

--- src/pages/meeting.jsx
+++ src/pages/meeting.jsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Cal from '@/components/Cal'
-import { useEffect,useRef } from "react";
+// import { useEffect,useRef } from "react";
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
