1135109db2496802bc7a5c2f824c99fa6852e8fe
David44357 <David44357@gmail.com>
2025-03-14 16:22:59 -0400

    Update meeting.jsx

--- src/pages/meeting.jsx
+++ src/pages/meeting.jsx
@@ -6,17 +6,8 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 export default function NewPage() {
-  const router = useRouter();
+return ( <main><cal /></main> )
 
-  useEffect(() => {
-    // Replace 'YOUR_REDIRECT_URL' with the actual URL you want to redirect to.
-    router.push('https://cal.com/ethicalwiki/meeting'); 
-  }, [router]);
-  return (
-    <div>
-      Loading calendar...
-    </div>
-  );
 }
 
 
