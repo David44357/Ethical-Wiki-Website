f88f7705c9f80ee84ca3848a5b12f5eb8ba3e2f5
David44357 <David44357@gmail.com>
2025-03-14 16:25:00 -0400

    Update meeting.jsx

--- src/pages/meeting.jsx
+++ src/pages/meeting.jsx
@@ -2,11 +2,24 @@ import Head from 'next/head'
 import Cal from '@/components/Cal'
 // import { useEffect,useRef } from "react";
 
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-
-export default function NewPage() {
-return ( <main><cal /></main> )
+import Cal, { getCalApi } from "@calcom/embed-react";
+  import { useEffect } from "react";
+  export default function MyApp() {
+	useEffect(()=>{
+	  (async function () {
+		const cal = await getCalApi({"namespace":"meeting"});
+		cal("ui", {"hideEventTypeDetails":false,"layout":"month_view"});
+	  })();
+	}, [])
+	return <Cal namespace="meeting"
+	  calLink="ethicalwiki/meeting"
+	  style={{width:"100%",height:"100%",overflow:"scroll",margin:"20px"}}
+	  config={{"layout":"month_view"}}
+    
+	  
+	/>;
+  };
+  
 
 }
 
