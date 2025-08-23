25c19d1e62dc63abe60d7eba9f2ff7161afa6a99
Muhammad Usama mashkkoor <usamamashkoor@gmail.com>
2025-08-23 09:50:47 +0500

    changing link

--- src/widgets/ctaWidget.jsx
+++ src/widgets/ctaWidget.jsx
@@ -6,7 +6,8 @@ export default function CtaWidget() {
                 <h2 className="text-3xl font-extrabold text-white sm:text-4xl">
                     <span className="block">Get Started</span>
                 </h2><div class="grid grid-cols-2 gap-4">
-                <a href="/meeting" className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-semibold rounded-md brand-color bg-white hover:bg-indigo-50 sm:w-auto">Schedule a consultation</a>
+                <a href="https://ew-consult.flutterflow.app/ " className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-semibold rounded-md brand-color bg-white hover:bg-indigo-50 sm:w-auto">Schedule a consultation</a>
+                {/* <a href="/meeting" className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-semibold rounded-md brand-color bg-white hover:bg-indigo-50 sm:w-auto">Schedule a consultation</a> */}
                 <a href="https://ew-new-page-assessment.flutterflow.app/" className="mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-semibold rounded-md brand-color bg-white hover:bg-indigo-50 sm:w-auto">New page assessment</a>
             </div></div>
         </div>
